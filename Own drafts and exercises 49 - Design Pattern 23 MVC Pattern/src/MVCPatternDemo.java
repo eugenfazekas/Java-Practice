cc4c90b9b689d4f3b561041273b422bc1e03f727
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-05-12 17:55:56 +0300

    mvc pattern

---
+++ Own drafts and exercises 49 - Design Pattern 23 MVC Pattern/src/MVCPatternDemo.java
@@ -0,0 +1,25 @@
+
+public class MVCPatternDemo {
+
+	public static void main(String[] args) {
+		
+		Student model = retriveStudentFromDatabase();
+		
+		StudentView studentView = new StudentView();
+		
+		StudentController controller = new StudentController(model, studentView);
+		
+		controller.updateView();
+		
+		controller.setStudentName("John");
+		
+		controller.updateView();
+	}
+	
+	private static Student retriveStudentFromDatabase() {
+		Student student = new Student();
+		student.setName("Robert");
+		student.setRollNo("10");
+		return student;
+	}
+}
