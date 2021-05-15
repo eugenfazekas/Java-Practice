e76b408fbed4f41ef7fc55008ff548219c81045c
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-05-15 15:49:31 +0300

    tranfer object pattern

---
+++ Own drafts and exercises 56 - Design Pattern 29 Transfer Object Pattern/src/StudentBO.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentBO {
+	
+	List<StudentVO> students;
+	
+	public StudentBO() {
+		students = new ArrayList<StudentVO>();
+		StudentVO student1 = new StudentVO("Robert", 0);
+		StudentVO student2 = new StudentVO("John", 1);
+		students.add(student1);
+		students.add(student2);
+	}
+	
+	public StudentVO getStudent(int rollNo) {
+
+		return students.get(rollNo);
+	}
+	
+	public List<StudentVO> getAllStudents() {
+		return students;
+	}
+	
+	public void deleteStudent(StudentVO student) {
+		students.remove(student);
+		System.out.println("Student; Roll No " + student.getRollNo() + " deleted from the database");	
+		}
+	
+	public void updateStudent(StudentVO student) {
+		
+		students.get(student.getRollNo()).setName(student.getName());
+		System.out.println("Student; Roll No " + student.getRollNo() + " updated in the database");
+	}
+}
